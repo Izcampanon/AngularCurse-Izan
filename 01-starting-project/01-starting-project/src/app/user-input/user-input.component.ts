e24e76d1166b859d040b1bf9200c8fb927f77a9b
Izcampanon <izcampanon@alumnos.unex.es>
2026-09-17 13:08:41 +0200

    Organizar repo: añadir first-angular-app e investment-calculator

---
+++ 01-starting-project/01-starting-project/src/app/user-input/user-input.component.ts
@@ -0,0 +1,31 @@
+import { Component , EventEmitter, Output} from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import type { InvestmentInput } from '../investment-results/investment-results.model';
+
+@Component({
+  selector: 'app-user-input',
+  standalone: true,
+  imports: [FormsModule],
+  templateUrl: './user-input.component.html',
+  styleUrl: './user-input.component.css'
+})
+export class UserInputComponent {
+
+  @Output() calculate = new EventEmitter<InvestmentInput>();
+
+  enteredInitialInvestment='0';
+  enteredAnnualInvestment='0';
+  enteredExpectedReturn='0';
+  enteredDuration='0';
+
+
+  onSubmit(){
+    this.calculate.emit({
+      initialInvestment:+this.enteredInitialInvestment, //el + convierte el string en un numero
+      annualInvestment: +this.enteredAnnualInvestment, 
+      expectedReturn: +this.enteredExpectedReturn, 
+      duration:+this.enteredDuration
+    });
+  }
+
+}
